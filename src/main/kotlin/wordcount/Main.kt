674b10313d0f8a529682d819b62129ada3ab4ef2
Ayan Ahmedov <Ayan.Ahmedov@erstegroup.com>
2021-08-24 09:46:39 +0200

    implementation of iteration 1

---
+++ src/main/kotlin/wordcount/Main.kt
@@ -0,0 +1,7 @@
+import wordcount.App
+
+fun main(args: Array<String>) {
+    val app = App(System.`in`, System.out)
+    app.run()
+}
+
---
+++ src/test/kotlin/wordcount/AppTestIT.kt
@@ -0,0 +1,23 @@
+package wordcount
+
+import wordcount.App
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+
+class AppTestIT {
+
+    @Test
+    fun `WordCounterApp reads input and writes the word count`(){
+        val outputBuffer = ByteArrayOutputStream()
+        val app = App(
+            inStream = "Mary had a little lamb".byteInputStream(),
+            outStream = outputBuffer
+        )
+
+        app.run()
+
+        val actualOutput = outputBuffer.toString()
+        Assertions.assertEquals("5", actualOutput)
+    }
+}
